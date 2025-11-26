d6f6095035eca296c98ac9a7923cfa39df061d1d
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-26 22:24:59 +0300

    fetch data JS

---
+++ fetch-data.js
@@ -0,0 +1,20 @@
+function fetchUserdata() {
+    const apiURL = 'https://jsonplaceholder.typicode.com/users';
+    document.getElementById('api-data').style.display = 'datacontainer';
+
+    async function fetchUserdata(apiURL) {
+        try {
+            const response = await response.json(); 
+        }
+}
+  dataContainer.innerHTML = 'Loading user data...';
+
+  <ul>
+    <li>user's name</li>
+    <li>userList</li>
+  </ul>
+  datacontainer.style.display = 'userList';
+
+  dataContainer.innerHTML = 'Failed to load user data.';
+
+  document.addEventListener('DOMContentLoaded', fetchUserdata);atch(apiURL);
